2af332a11fa7c95ac18de8327db97b819e65a977
Valts Vilcans <valts03@inbox.lv>
2020-06-11 15:47:54 +0200

    [fetch data on update]

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,24 +9,45 @@ import "./Blog.css";
 class Blog extends Component {
   state = {
     posts: [],
+    selectedPostId: null,
   };
 
   componentDidMount() {
     axios.get("https://jsonplaceholder.typicode.com/posts").then((res) => {
-      this.setState({ posts: res.data });
+      const posts = res.data.slice(0, 4);
+      const updatedPosts = posts.map((post) => {
+        return {
+          ...post,
+          author: "Max",
+        };
+      });
+      this.setState({ posts: updatedPosts });
     });
   }
 
+  postSelected = (id) => {
+    this.setState({ selectedPostId: id });
+  };
+
   render() {
     const posts = this.state.posts.map((post) => {
-      return <Post key={post.id} title={post.title} />;
+      return (
+        <Post
+          key={post.id}
+          title={post.title}
+          author={post.author}
+          clicked={() => {
+            this.postSelected(post.id);
+          }}
+        />
+      );
     });
 
     return (
       <div>
         <section className="Posts">{posts}</section>
         <section>
-          <FullPost />
+          <FullPost id={this.state.selectedPostId} />
         </section>
         <section>
           <NewPost />
